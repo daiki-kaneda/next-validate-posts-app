76aad3f4e2d7674a89405fc07ace511d2efdbd96
daiki-kaneda <daiki0622developing@gmail.com>
2025-12-21 01:58:40 +0900

    Add DTO for Post,CreatePostInput with validation rule by zod

---
+++ lib/schemas/post.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const PostSchema = z.object({
+    userId: z.number(),
+    id: z.number(),
+    title: z.string().min(1, "タイトルは必須です。"),
+    body: z.string().min(1, "本文は必須です。"),
+});
+
+export const CreatePostSchema = PostSchema.omit({ id: true });
+
+// 型の抽出
+export type Post = z.infer<typeof PostSchema>;
+export type CreatePostInput = z.infer<typeof CreatePostSchema>;
+
