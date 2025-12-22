765b0b2d35b778955424d9b7616a73b83dee78d5
daiki-kaneda <daiki0622developing@gmail.com>
2025-12-22 23:00:03 +0900

    Add loading.tsx for loading view

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+    </div>
+  );
+}
